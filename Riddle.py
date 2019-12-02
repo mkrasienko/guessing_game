429660014e11d4f38e8dc2855510862efcf12bf7
mkrasienko <mkrasienko@gmail.com>
2019-12-02 13:57:16 +0100

    Sorted the issue of finding the corret word in answer string by adding a regex pattern

--- Riddle.py
+++ Riddle.py
@@ -1,42 +1,43 @@
 #This is a guessing game. It uses the ridddle from the 1995 Batman Forever movie. It will give the user 3 chances to guess the answer. It will not accept a blank answer. If the user fails to guess correctly in 3 tries, they will be given a chance to play again.
-def riddle(): #function checks users answer and returns tuple with indication if user ran out of guesses or won
+import re
+import time
+
+def riddle(): #function checks users answer and returns boolean of variable out_of _guesses
     out_of_guesses = False
-    secret_word: str = "bat"
+    secret_word = 'bat'
     guess_count = 0
-    win = False
-    guess = ""
-    while secret_word not in guess and not out_of_guesses:
-        guess = input("\nEnter a guess: ")
-        if guess == "": #checks for blank answer
-            print("You have to enter an answer. Please try again.")
-        elif guess_count == 0 and "bat" not in guess and guess != "":
+    guess = ''
+    answer = None
+    while answer == None and not out_of_guesses:
+        guess = input('\nEnter a guess: ').lower()
+        answer = re.search(r'\bbat\b', guess, re.I)
+        if guess == '': #checks for blank answer
+            print('You have to enter an answer. Please try again.')
+        elif guess_count == 0 and answer == None:
             guess_count += 1
-            print(f"That's not it! Try again. You have 2 guesses left")
-        elif guess_count == 1 and "bat" not in guess and guess != "":
-            print("That's not it! Last chance.")
+            print('That\'s not it! Try again. You have 2 guesses left')
+        elif guess_count == 1 and answer == None:
+            print('That\'s not it! Last chance.')
             guess_count += 1
-        elif guess_count == 2 and "bat" not in guess and guess != "":
+        elif guess_count == 2 and answer == None:
             out_of_guesses = True
-        elif secret_word in guess:
-            win = True
-    return out_of_guesses, win
+        else:
+            out_of_guesses = False
+    return out_of_guesses
 
 
-print("\nI see without seeing. To me, darkness is as clear as daylight. What am I? - You have 3 tries.")
+print('\nI see without seeing. To me, darkness is as clear as daylight. What am I? - You have 3 tries.')
 
-status = riddle()
+wrong_answer = riddle()
 
-while status == (True, False):
-    print("WRONG! You're out of guesses. YOU LOSE!")
-    play_again = input("--\nWant to play again? (Y/N): ")
-    if play_again in ["y", "Y"]:
-        status = riddle()
-        if status == (False, True):
-            break
-    elif play_again not in ["y", "Y"]:
+while wrong_answer == True:
+    print('WRONG! You\'re out of guesses. YOU LOSE!')
+    time.sleep(1)
+    play_again = input('--\nWant to play again? (Y/N): ')
+    if play_again in ['y', 'Y']:
+        wrong_answer = riddle()
+    else:
         print('Bye! Bye!')
         break
-    elif status == (False, True):
-        break
-if status == (False, True):
-    print("Good guess. YOU WIN!")
+if wrong_answer == False:
+    print('Good guess. YOU WIN!')
